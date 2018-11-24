06c4339772f0417a065a5dd56a719b34db3adb7e
Jtorres8 <jhon.torres.alva@hotmail.com>
2018-11-24 04:22:24 -0500

    Atras y Siguiente

--- app/src/main/java/com/example/jhontorresalva/miprimerapp/two.java
+++ app/src/main/java/com/example/jhontorresalva/miprimerapp/two.java
@@ -1,7 +1,9 @@
 package com.example.jhontorresalva.miprimerapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class two extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class two extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two);
+    }
+    public void antes(View view){
+        Intent atras = new Intent(this, MainActivity.class );
+        startActivity(atras);
     }
 }
